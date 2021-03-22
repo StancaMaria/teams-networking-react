fc7a0ceecfadbd496f42e15b72350f13bdd192df
Stanca Stirb <stancastirb@gmail.com>
2021-03-22 19:17:40 +0200

    add my stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
+import { PersonTable } from './PersonTable';
 import './App.css';
+
+const teams = [
+  {
+      id: "a123",
+      firstName: "Stanca",
+      lastName: "Stirb",
+      gitHub: "StancaMaria"
+  }, 
+  {
+      id:"b564",
+      firstName:"Matei",
+      lastName:"Nicolae",
+      gitHub: "nmatei",
+   }
+];
 
 function App() {
   return (
     <div>
       <h1>Teams Networking</h1>
-      <div>Search</div>
-      <p>table...</p>
+    <PersonTable teams={teams} />
     </div>
   );
 }
